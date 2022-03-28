1ee001ae91301616e4f990fcaa024902f1e4357e
Jimmy Hoang <dunghd296@gmail.com>
2022-03-29 00:48:57 +0700

    add delete, edit and tag for current user

--- interactive-comments-section-main/script.js
+++ interactive-comments-section-main/script.js
@@ -2,9 +2,12 @@
 function renderUI (data) {
     let wrapper = document.querySelector('.wrapper')
     let comments = data.comments
+    let currentUser = data.currentUser
+
+    console.log ('username is' + currentUser.username)
     console.log(comments)
     comments.forEach(comment => {
-        addComment(wrapper,comment)
+        addComment(wrapper,comment,currentUser)
         let replies = comment.replies
         
         console.log (comment.id)
@@ -12,7 +15,7 @@ function renderUI (data) {
             let replyNode = document.createElement('div')
             replyNode.className = 'reply-card'
             replies.forEach(reply => {
-                addComment(replyNode,reply)
+                addComment(replyNode,reply,currentUser)
             })
             console.log(replyNode)
             wrapper.appendChild(replyNode)
@@ -21,7 +24,10 @@ function renderUI (data) {
     });
 }
 
-function addComment (wrapper,comment){
+function addComment (wrapper,comment,currentUser){
+    let isCurrentUserHide = `style="display:${currentUser.username==comment.user.username?"none":""}"`
+    let isCurrentUserShow = `style="display:${currentUser.username==comment.user.username?"":"none"}"`
+    
     wrapper.innerHTML = wrapper.innerHTML + 
     `
     <div class="comment-card">
@@ -30,9 +36,11 @@ function addComment (wrapper,comment){
             <img src=${comment.user.image.png} alt="">
             </div>
             <h3 class="user-name">${comment.user.username}</h3>
+            <span class="user-current" ${isCurrentUserShow} >you</span>
             <h3 class="comment-time">${comment.createdAt}</h3>
         </div>
         <p class="comment">
+            <span class="replyTo" style="display:${comment.replyingTo?'unset':'none'}">@${comment.replyingTo}</span>
             ${comment.content}      
         </p>
         <div class="vote">
@@ -42,10 +50,22 @@ function addComment (wrapper,comment){
             <input type="radio" name="vote" id="minus">
             <label for="minus"><img src="./images/icon-minus.svg" alt="minus"></label>
         </div>
-        <button class="reply">
+
+        <div class="action-button">
+          
+          <button class="delete" ${isCurrentUserShow}>
+            <img src="./images/icon-delete.svg" alt="delete">
+            <h3>Delete</h3>
+          </button>
+          <button class="edit" ${isCurrentUserShow}>
+            <img src="./images/icon-edit.svg" alt="edit">
+            <h3>Edit</h3>
+          </button>
+          <button class="reply" ${isCurrentUserHide}>
             <img src="./images/icon-reply.svg" alt="reply">
             <h3>Reply</h3>
-        </button>
+          </button>
+        </div>
     </div>
     `
 }
@@ -54,4 +74,7 @@ fetch("./data.json")
 .then(response => {
     return response.json()
 })
-.then(jsondata => renderUI(jsondata))
+.then(jsondata => {
+    renderUI(jsondata)
+    console.log(jsondata)
+})
